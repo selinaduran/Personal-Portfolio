885fa792c0a8e3510e6b0a71ca6317c28af54aeb
selinaduran <selinad177@gmail.com>
2022-11-15 20:11:47 -0600

    added details to home page

--- personal_portfolio/pages/index.js
+++ personal_portfolio/pages/index.js
@@ -12,14 +12,38 @@ export default function Home() {
 
       <main className={styles.main}>
         <div>
-          <h1 className={styles.title}>
-            Hi there! I'm Selina. Welcome to my tech corner!
-          </h1>
+          <h1 className={styles.title}>Selina Duran</h1>
+          <p>profile pic would go here</p>
+          <h2>Hi there! I'm Selina. Welcome to my tech corner!</h2>
+          <p>personal site links (github/linkedin) would go here</p>
+        </div>
+        <div>
+          <ul>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
         </div>
         <div className={styles.grid}>
           <h2>Selina Duran</h2>
           <h2>Full-Stack Software Engineer</h2>
+          <p>self photo would go here</p>
           <p>Hello! I am a former educator and school psychologist who eventually switched over to tech! I am currently a full-stack software engineer who enjoys learning new programming languages, creating optimal user interfaces within frontend applications, and handling databases to produce well-balanced applications. I thrive in environments where there are opportunities for team collaboration, problem-solving and professional growth. I would love to join a team to share my experiences with in order to help achieve their company goals. Feel free to reach out if you think I would be a good fit for your team</p>
+          <button>View Portfolio</button>
+          <button>View Resume</button>
+        </div>
+        <div className={styles.grid}>
+          <h2>Tech Stacks I Am Proficient In:</h2>
+          <p>tech stack icons would go here</p>
+        </div>
+        <div className={styles.grid}>
+          <h2>Tech Stacks I Have Experience With:</h2>
+          <p>tech stack icons would go here</p>
+        </div>
+        <div className={styles.grid}>
+          <h2>Featured Projects</h2>
+          <p>featured projects would go here</p>
         </div>
       </main>
 
