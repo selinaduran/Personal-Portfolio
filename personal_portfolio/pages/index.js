785a2be0e0a997a1cb14162cd6c1c980126e47ae
selinaduran <selinad177@gmail.com>
2022-11-16 19:45:31 -0600

    rendered photos onto homepage

--- personal_portfolio/pages/index.js
+++ personal_portfolio/pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <div>
           <h1 className={styles.title}>Selina Duran</h1>
+          <img src="/img/ProfilePic.jpeg" alt="profile pic"/>
           <h2>Hi there! I'm Selina. Welcome to my tech corner!</h2>
           <p>personal site links (github/linkedin) would go here</p>
         </div>
@@ -27,7 +28,7 @@ export default function Home() {
         <div className={styles.grid}>
           <h2>Selina Duran</h2>
           <h2>Full-Stack Software Engineer</h2>
-          <p>self photo would go here</p>
+          <img src="/img/AboutMe.jpeg" alt="about me"/>
           <p>Hello! I am a former educator and school psychologist who eventually switched over to tech! I am currently a full-stack software engineer who enjoys learning new programming languages, creating optimal user interfaces within frontend applications, and handling databases to produce well-balanced applications. I thrive in environments where there are opportunities for team collaboration, problem-solving and professional growth. I would love to join a team to share my experiences with in order to help achieve their company goals. Feel free to reach out if you think I would be a good fit for your team</p>
           <button>View Portfolio</button>
           <button>View Resume</button>
