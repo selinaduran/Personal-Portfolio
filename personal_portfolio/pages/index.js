a1da9d586ad2fc36e3c86358a6ec389d9eb10ab2
selinaduran <selinad177@gmail.com>
2022-11-16 20:52:10 -0600

    adjusted aboutme photo and added css styles

--- personal_portfolio/pages/index.js
+++ personal_portfolio/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         <div className={styles.grid}>
           <h2>Selina Duran</h2>
           <h2>Full-Stack Software Engineer</h2>
-          <img src="/img/AboutMe.jpeg" alt="about me"/>
+          <img className={styles.aboutme} src="/img/AboutMe.jpeg" alt="about me"/>
           <p>Hello! I am a former educator and school psychologist who eventually switched over to tech! I am currently a full-stack software engineer who enjoys learning new programming languages, creating optimal user interfaces within frontend applications, and handling databases to produce well-balanced applications. I thrive in environments where there are opportunities for team collaboration, problem-solving and professional growth. I would love to join a team to share my experiences with in order to help achieve their company goals. Feel free to reach out if you think I would be a good fit for your team</p>
           <button>View Portfolio</button>
           <button>View Resume</button>
