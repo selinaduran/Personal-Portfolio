01e4a2d5f8be072392051fe6290dbcc672977ca1
selinaduran <selinad177@gmail.com>
2022-11-25 00:11:04 -0600

    made container sections for flex positioning

--- personal_portfolio/pages/index.js
+++ personal_portfolio/pages/index.js
@@ -11,39 +11,43 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>Selina Duran</h1>
-          <img className={styles.profilepic} src="/img/ProfilePic.jpeg" alt="profile pic"/>
-          <h2>Hi there! I'm Selina. Welcome to my tech corner!</h2>
-          <p>personal site links (github/linkedin) would go here</p>
+        <div className={styles.container1}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>Selina Duran</h1>
+            <img className={styles.profilepic} src="/img/ProfilePic.jpeg" alt="profile pic" />
+            <h2>Hi there! I'm Selina. Welcome to my tech corner!</h2>
+            <p>personal site links (github/linkedin) would go here</p>
+          </div>
+          <div className={styles.links}>
+            <ul>
+              <li>About Me</li>
+              <li>Portfolio</li>
+              <li>Resume</li>
+              <li>Contact</li>
+            </ul>
+          </div>
         </div>
-        <div className={styles.links}>
-          <ul>
-            <li>About Me</li>
-            <li>Portfolio</li>
-            <li>Resume</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <div className={styles.about}>
-          <h2>Selina Duran</h2>
-          <h2>Full-Stack Software Engineer</h2>
-          <img className={styles.aboutme} src="/img/AboutMe.jpeg" alt="about me"/>
-          <p>Hello! I am a former educator and school psychologist who eventually switched over to tech! I am currently a full-stack software engineer who enjoys learning new programming languages, creating optimal user interfaces within frontend applications, and handling databases to produce well-balanced applications. I thrive in environments where there are opportunities for team collaboration, problem-solving and professional growth. I would love to join a team to share my experiences with in order to help achieve their company goals. Feel free to reach out if you think I would be a good fit for your team</p>
-          <button>View Portfolio</button>
-          <button>View Resume</button>
-        </div>
-        <div className={styles.proficient}>
-          <h2>Tech Stacks I Am Proficient In:</h2>
-          <p>tech stack icons would go here</p>
-        </div>
-        <div className={styles.experience}>
-          <h2>Tech Stacks I Have Experience With:</h2>
-          <p>tech stack icons would go here</p>
-        </div>
-        <div className={styles.featured}>
-          <h2>Featured Projects</h2>
-          <p>featured projects would go here</p>
+        <div className={styles.container2}>
+          <div className={styles.about}>
+            <h2>Selina Duran</h2>
+            <h2>Full-Stack Software Engineer</h2>
+            <img className={styles.aboutme} src="/img/AboutMe.jpeg" alt="about me" />
+            <p>Hello! I am a former educator and school psychologist who eventually switched over to tech! I am currently a full-stack software engineer who enjoys learning new programming languages, creating optimal user interfaces within frontend applications, and handling databases to produce well-balanced applications. I thrive in environments where there are opportunities for team collaboration, problem-solving and professional growth. I would love to join a team to share my experiences with in order to help achieve their company goals. Feel free to reach out if you think I would be a good fit for your team</p>
+            <button>View Portfolio</button>
+            <button>View Resume</button>
+          </div>
+          <div className={styles.proficient}>
+            <h2>Tech Stacks I Am Proficient In:</h2>
+            <p>tech stack icons would go here</p>
+          </div>
+          <div className={styles.experience}>
+            <h2>Tech Stacks I Have Experience With:</h2>
+            <p>tech stack icons would go here</p>
+          </div>
+          <div className={styles.featured}>
+            <h2>Featured Projects</h2>
+            <p>featured projects would go here</p>
+          </div>
         </div>
       </main>
 
